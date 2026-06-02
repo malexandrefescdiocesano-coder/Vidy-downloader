ad4d54c996f5b7d6792b4b8e2e05960ac800ac84
malexandrefescdiocesano-coder <malexandre.fescdiocesano@gmail.com>
2026-06-02 13:45:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ if not st.session_state.logado:
     st.stop()
 
 # INTERFACE DO APP COMPLETO
-st.warning("🚨 AVISO DE SEGURANÇA:
+st.warning("🚨 AVISO DE PRIVACIDADE:
 
            Este é um app privado só para você. NÃO compartilhe este link com ninguém!🔗🚫
            
